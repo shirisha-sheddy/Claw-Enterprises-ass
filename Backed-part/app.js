31caec601d5f82683e78087ebd0eb2c678815bfe
shirisha-sheddy <shirishasheddy963@gmail.com>
2024-08-03 16:22:48 +0530

    last commit

--- Backed-part/app.js
+++ Backed-part/app.js
@@ -19,7 +19,12 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
+app.get("/", (req, res) => {
+   res.json({
+    success: true,
+    message: "GO BACK TO HOME",
+   });
+});
 
 app.use("/api/v1/message", messageRouter);
 
